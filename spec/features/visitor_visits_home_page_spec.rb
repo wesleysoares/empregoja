aa5c2d77c9088bd133749aea18e494d2f57f8f65
Joao Almeida <joaorsalmeida@gmail.com>
2015-09-08 16:54:19 -0300

    Visitor should see jobs on home page

--- spec/features/visitor_visits_home_page_spec.rb
+++ spec/features/visitor_visits_home_page_spec.rb
@@ -5,4 +5,16 @@ feature 'Visitor visits Emprego Ja home page' do
     visit root_path
     expect(page).to have_content('Emprego Já')
   end
+
+  scenario 'and see jobs' do
+    Job.create(title: 'Vaga de Dev',
+               category: 'Desenvolvedor',
+               company: 'Campus Code',
+               description: 'Dev Junior Rails com ao menos um projeto',
+               location: 'São Paulo')
+    visit root_path
+    expect(page).to have_content('Vaga de Dev')
+    expect(page).to have_content('Campus Code')
+    expect(page).to have_content('São Paulo')
+  end
 end
