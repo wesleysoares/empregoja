0e615025326c64fb3ee6674728718c332475becd
Joao Almeida <joaorsalmeida@gmail.com>
2015-09-10 17:34:51 -0300

    User should choose a previously created company

--- spec/features/user_creates_new_job_spec.rb
+++ spec/features/user_creates_new_job_spec.rb
@@ -3,16 +3,21 @@ require 'rails_helper'
 feature 'User creates a new job' do
 
   scenario 'successfully' do
+    company = Company.create(name:     'Campus Code',
+                          location: 'São Paulo',
+                          mail:     'contato@campuscode.com.br',
+                          phone:    '2369-3476')
+
     job = Job.new(title:    'Dev Master',
                   location: 'Rio de Janeiro',
                   category: 'Desenvolvedor',
-                  company:  'Campus Code',
                   description: 'Vaga para Dev Master para o Bootcamp Rails')
+
     visit new_job_path
     fill_in 'Title',       with: job.title
     fill_in 'Location',    with: job.location
     fill_in 'Category',    with: job.category
-    fill_in 'Company',     with: job.company
+    select  'Campus Code'
     fill_in 'Description', with: job.description
 
     click_on 'Criar Vaga'
@@ -25,16 +30,21 @@ feature 'User creates a new job' do
   end
 
   scenario 'featured job' do
+    company = Company.create(name:     'Campus Code',
+                          location: 'São Paulo',
+                          mail:     'contato@campuscode.com.br',
+                          phone:    '2369-3476')
+
     job = Job.new(title:    'Dev Master',
                   location: 'Rio de Janeiro',
                   category: 'Desenvolvedor',
-                  company:  'Campus Code',
                   description: 'Vaga para Dev Master para o Bootcamp Rails')
     visit new_job_path
+
     fill_in 'Title',       with: job.title
     fill_in 'Location',    with: job.location
     fill_in 'Category',    with: job.category
-    fill_in 'Company',     with: job.company
+    select  'Campus Code'
     fill_in 'Description', with: job.description
     check   'Featured'
 
