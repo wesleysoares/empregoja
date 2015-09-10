ecf3109dd8063fdf9e5931ac90b6bfe212a1bf6b
Sergio Wesley <sw@campus.com>
2015-09-09 22:25:31 -0300

    Visitante ve detalhes de uma vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,7 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+  def show
+    @job = Job.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :jobs, only: [:show]
   root 'jobs#index'
 end
