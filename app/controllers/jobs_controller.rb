274c211072d4a872277498a1432adefb4dd1a306
Sergio Wesley <sw@campus.com>
2015-09-10 21:04:36 -0300

    Usuario cadastra vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,9 @@
 class JobsController < ApplicationController
+
+  def new
+    @job = Job.new
+  end
+
   def index
     @jobs = Job.all
   end
@@ -6,4 +11,10 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def create
+    @job = Job.create(params.require(:job).permit(:title, :company, :description, :category, :location))
+    redirect_to @job
+  end
+
 end
