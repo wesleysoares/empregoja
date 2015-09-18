1e671ffb2b70db95982bf2070fa4428dbd9aaaa2
Sergio Wesley <sw@campus.com>
2015-09-17 22:24:16 -0300

    Usuário edita vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,17 @@
 class JobsController < ApplicationController
+before_action :set_job, only:[:show, :edit, :update]
+before_action :set_collection, only:[:index, :new, :create, :edit]
+
+
   def index
     @jobs = Job.all
-    @companies = Company.all
-    @categories = Category.all
-  end
+    end
 
   def show
-    @job = Job.find(params[:id])
   end
 
   def new
     @job = Job.new
-    @companies = Company.all
-    @categories = Category.all
   end
 
   def create
@@ -21,25 +20,28 @@ class JobsController < ApplicationController
       redirect_to @job
     else
       flash[:error] = "Warning! All fields are mandatory."
-      @companies = Company.all
-      @categories = Category.all
       render 'new'
     end
   end
 
   def edit
-    @job = Job.find(params[:id])
-    @companies = Company.all
-    @categories = Category.all
   end
 
   def update
-    @job = Job.find(params[:id])
     @job.update(job_params)
     redirect_to @job
   end
 
   private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def set_collection
+    @companies = Company.all
+    @categories = Category.all
+  end
 
   def job_params
     params.require(:job).permit(:title, :location, :category_id, :company_id, :description, :featured)
