04993553cf2c1448404f5de35415bd9bc988d020
Sergio Wesley <sw@campus.com>
2015-09-21 20:03:47 -0300

    Usuario cadatra categorias

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,16 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+  def new
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(params.require(:category).permit(:name))
+    if(@category.save)
+      redirect_to @category
+    else
+      flash[:error]="Warning! All fields are mandatory."
+      render 'new'
+    end
+  end
 end
