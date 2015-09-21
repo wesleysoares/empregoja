c44c40a3dcaa23fba40761e47b51c860ca81468b
Joao Almeida <joaorsalmeida@gmail.com>
2015-09-20 22:40:21 -0300

    Visitor see recent jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,14 @@
 class Job < ActiveRecord::Base
   belongs_to :company
   belongs_to :category
-  
+
   validates :title, :location, :description, :category, :company, presence: true
 
   def featured?
     featured
+  end
+
+  def recent?
+    DateTime.now < (created_at + 5.days)
   end
 end
