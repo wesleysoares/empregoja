10d3e82c15903191663bd1dba20eaed464372a69
Joao Almeida <joaorsalmeida@gmail.com>
2015-09-20 23:31:51 -0300

    Visitor should not see expired jobs

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -7,28 +7,67 @@ RSpec.describe Job, type: :model do
     context "created today" do
       it "is recent" do
         job = create_job
-        expect(job.recent?).to be true
+        expect(job).to be_recent
       end
     end
 
     context "created 6 days ago" do
       it "is not recent" do
         travel_to 6.days.ago do
-          job = create_job
-          expect(job.recent?).to be false
+          @job = create_job
         end
+        expect(@job).not_to be_recent
       end
     end
 
     context "created 4 days ago" do
       it "is recent" do
         travel_to 4.days.ago do
-          job = create_job
-          expect(job.recent?).to be true
+          @job = create_job
         end
+        expect(@job).to be_recent
       end
     end
 
   end
+
+  describe "#expired?" do
+
+    context "created today" do
+      it "is not expired" do
+        job = create_job
+        expect(job).not_to be_expired
+      end
+    end
+
+    context "created 89 days ago" do
+      it "is not expired" do
+        travel_to 89.days.ago do
+          @job = create_job
+        end
+        expect(@job).not_to be_expired
+      end
+    end
+
+    context "created 90 days ago" do
+      it "is expired" do
+        travel_to 90.days.ago do
+          @job = create_job
+        end
+        expect(@job).to be_expired
+      end
+    end
+
+    context "created 91 days ago" do
+      it "is expired" do
+        travel_to 91.days.ago do
+          @job = create_job
+        end
+        expect(@job).to be_expired
+      end
+    end
+
+  end
+
 
 end
