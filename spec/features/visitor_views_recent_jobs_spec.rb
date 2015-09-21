deb3d25266df5dab130fd78004b0355e7449d1b8
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-09-21 00:26:29 -0300

    Change css first to first-child

--- spec/features/visitor_views_recent_jobs_spec.rb
+++ spec/features/visitor_views_recent_jobs_spec.rb
@@ -18,7 +18,7 @@ feature 'Visitor view recent jobs' do
     visit root_path
 
     # primeira elemento com o css "jobs" da pagina
-    within('.jobs:first') do
+    within('.jobs:first-child') do
       expect(page).to have_content('Novidade')
     end
   end
