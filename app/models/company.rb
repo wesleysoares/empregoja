fe0c7e480e3d30bc767b2b0cf0e338ec4cab9d32
Sergio Wesley <sw@campus.com>
2015-09-22 19:50:09 -0300

    Destacar empresas com muitas vagas

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,10 @@
 class Company < ActiveRecord::Base
   has_many :jobs
   validates :name, :location, :phone, :mail, presence: true
+
+  def premium?
+    if jobs.count >= 5
+      true
+    end
+  end
 end
