05c7e9bfe169a3118d6c3d7fefb3d09de5b75ce5
Sergio Wesley <sw@campus.com>
2015-09-21 22:12:47 -0300

    Destacar vagas recentes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,12 @@
 class Job < ActiveRecord::Base
   belongs_to :company
   belongs_to :category
-  
+
   validates :title, :location, :description, :category, :company, presence: true
+
+  def recent?
+    created_at > 5.days.ago
+  end
 
   def featured?
     featured
